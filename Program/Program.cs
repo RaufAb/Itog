305b62f4a0280c1c295192f770cb1537ba121eae
RaufAb <124442873+RaufAb@users.noreply.github.com>
2023-03-25 19:13:03 +0300

    Add files via upload

---
+++ Program/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Сколько элементов нужно ввести? ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arrayStrings = new string[size];
+int ln = 3;
+int zer = 0;
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент массива: ");
+    string e = Convert.ToString(Console.ReadLine());
+    if (e.Length <= ln)
+    {
+        arrayStrings[zer] = e;
+        zer++;
+    }
+}
+Console.WriteLine();
+PrintArray(arrayStrings);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
